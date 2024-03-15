4a3c5beaa7901e5276f933f2cfe31d82f6bb9b63
Sandro Pereira <sandro-pereira@live.com.pt>
2024-03-15 10:51:36 +0000

    V2

--- Logic-Apps/PowerShell-script-identify-LA-SQL-V2-actions-triggers/IdentifyAllLAWithSQLV2ActionsTriggers.ps1
+++ Logic-Apps/PowerShell-script-identify-LA-SQL-V2-actions-triggers/IdentifyAllLAWithSQLV2ActionsTriggers.ps1
@@ -12,45 +12,42 @@ Connect-AzAccount
 # Select the appropriate subscription if you have multiple subscriptions
 #Set-AzContext -SubscriptionId $subscriptionId
 
-# Function to extract actions recursively
-function Get-Actions {
+# Function to extract actions and triggers recursively
+function Get-ActionsAndTriggers {
     param (
         $node
     )
-    $actions = @()
-
+    $actionsAndTriggers = @()
     foreach ($key in $node.psobject.Properties.Name) {
         if ($node.$key.type -eq "ApiConnection") {
-            if ($node.$key.inputs.path -like "*/v2/datasets*" -and $node.$key.inputs.host.connection.name -like "*sql*") {
-                $actions += $key
+            if ($node.$key.inputs.path -like "*v2/datasets/*" -and $node.$key.inputs.host.connection.name -like "*sql*") {
+                $actionsAndTriggers += $key
             }
         } elseif ($node.$key -is [System.Management.Automation.PSCustomObject]) {
-            $actions += Get-Actions -node $node.$key
+            $actionsAndTriggers += Get-ActionsAndTriggers -node $node.$key
         }
     }
-
-    return $actions
+    return $actionsAndTriggers
 }
-
+ 
 # Retrieve all Logic Apps within the subscription
 $logicApps = Get-AzResource -ResourceType Microsoft.Logic/workflows
-
-# Iterate through each Logic App and extract actions
+ 
+# Iterate through each Logic App and extract actions and triggers
 foreach ($logicApp in $logicApps) {
     # Retrieve Logic App definition
     $logicAppDefinition = Get-AzResource -ResourceId $logicApp.ResourceId -ExpandProperties
-
-    # Extract actions from the Logic App definition
-    $allActions = Get-Actions -node $logicAppDefinition.Properties.Definition.actions
-
-    # Display the Logic App name if filtered actions were found
-    if ($allActions.Count -gt 0) {
-        Write-Host "Logic App: $($logicApp.Name) - RG: $($logicApp.ResourceGroupName)" -ForegroundColor Red
  
-        # Display the list of filtered actions
-        Write-Host "Filtered Actions:"
-
-        $allActions
+    # Extract actions and triggers from the Logic App definition
+    $allActionsAndTriggers = Get-ActionsAndTriggers -node $logicAppDefinition.Properties.Definition.triggers
+    $allActionsAndTriggers += Get-ActionsAndTriggers -node $logicAppDefinition.Properties.Definition.actions
+ 
+    # Display the Logic App name if filtered actions and triggers were found
+    if ($allActionsAndTriggers.Count -gt 0) {
+        Write-Host "Logic App: $($logicApp.Name) - RG: $($logicApp.ResourceGroupName)" -ForegroundColor Red
+        # Display the list of filtered actions and triggers
+        Write-Host "Filtered Actions and Triggers:"
+        $allActionsAndTriggers
         Write-Host ""
     }
 }
